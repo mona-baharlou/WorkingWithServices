aea829be224db50d4e9e61ecf52e62a101c05739
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-01 14:34:06 +0330

    Service updated

--- app/src/main/java/com/bahrlou/workingwithservices/LogService.kt
+++ app/src/main/java/com/bahrlou/workingwithservices/LogService.kt
@@ -3,6 +3,7 @@ package com.bahrlou.workingwithservices
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class LogService : Service() {
 
@@ -16,6 +17,8 @@ class LogService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        doYourJob()
+
         return START_STICKY //automatically service will be started if android system closes this service
 
         //return START_NOT_STICKY ////service will be started if android system closes this service
@@ -24,5 +27,11 @@ class LogService : Service() {
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    private fun doYourJob() {
+        for (i in 0..1000000) {
+            Log.d("", "doYourJob: testMyService --> number $i")
+        }
     }
 }
--- app/src/main/java/com/bahrlou/workingwithservices/MainActivity.kt
+++ app/src/main/java/com/bahrlou/workingwithservices/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bahrlou.workingwithservices
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        startMyService()
+    }
+
+    private fun startMyService() {
+        val intent = Intent(this, LogService::class.java)
+        startService(intent) //background Service
     }
 }
